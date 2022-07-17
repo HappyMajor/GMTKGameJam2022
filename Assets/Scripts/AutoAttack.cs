a866c8700b66fa72db17504f18d12e17c5580652
szTheory <szTheory@users.noreply.github.com>
2022-07-16 23:06:37 -0400

    Weapons have multiple levels now. You can configure their speed, range, duration, knockback distance, and damage

--- Assets/Scripts/AutoAttack.cs
+++ Assets/Scripts/AutoAttack.cs
@@ -7,10 +7,25 @@ public class AutoAttack : MonoBehaviour
     public float Damage { get; set; }
     public Vector3 Knockback { get; set; }
     public Vector3 Direction { get; set; }
+    public float Range { get; set; }
+    public float Speed { get; set; }
+    private Vector3 startingPosition;
 
     public void Start() {
+        // Keep track of starting position
+        startingPosition = transform.position;
+
         // Rotation in the direction
         transform.rotation = Quaternion.LookRotation(Vector3.forward, Direction);
+    }
+
+    public void FixedUpdate() {
+        transform.position += Direction * Time.fixedDeltaTime * Speed;
+
+        // Destroy if the attack's range is exceeded
+        if (Vector3.Distance(startingPosition, transform.position) > Range) {
+            Destroy(gameObject);
+        }
     }
 
 
--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,20 +2,54 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum AttackLevel {
+    One, Two, Three
+}
+
+public struct AttackLevelStats
+{
+  public AttackLevelStats(
+    float speed,
+    float range,
+    float duration,
+    float knockback,
+    int damage
+    )
+  {
+    Duration = duration;
+    Knockback = knockback;
+    Range = range;
+    Speed = speed;
+    Damage = damage;
+  }
+
+  public float Duration { get; }
+  public float Knockback { get; }
+  public float Range { get; }
+  //defined as attacks per second
+  public float Speed { get; }
+  public float Damage { get; }
+}
+
 public class PlayerController : MonoBehaviour
 {
+    private IDictionary<AttackLevel, AttackLevelStats> weaponLevelStats = new Dictionary<AttackLevel, AttackLevelStats>()
+    {
+        { AttackLevel.One, new AttackLevelStats(speed: 1.5f, range: 1.0f, duration: 0.05f, knockback: 1.0f, damage: 1) },
+        { AttackLevel.Two, new AttackLevelStats(speed: 5.0f, range: 4.0f, duration: 1.00f, knockback: 1.4f, damage: 1) },
+    };
+    [SerializeField] private AttackLevel currentWeaponLevel = AttackLevel.One;
     public float moveSpeed;
     public float autoAttackMovementDelay;
-    //defined as attacks per second
-    public float attackSpeed;
-    public float attackRange;
-    public float attackDuration;
-    public float attackKnockback;
-
     public GameObject autoAttackPrefab;
 
     private float lastAuttoAttackTime;
     private bool isMovementBlockedByAttack = false;
+    private Vector3 playerBounds;
+
+    public void Start() {
+        playerBounds = GetComponent<SpriteRenderer>().bounds.extents;
+    }
 
     public void Update()
     {
@@ -71,32 +105,51 @@ public class PlayerController : MonoBehaviour
         mouseWorldPos.z = transform.position.z;
         Vector3 mouseDirection = (mouseWorldPos - transform.position).normalized;
 
+        // Attack stats
+        var stats = getAttackStats();
+
         // Create an autoattack
-        AutoAttack autoAttack = Instantiate(autoAttackPrefab, transform.position + mouseDirection * attackRange, Quaternion.Euler(new Vector3(0, 0, 0)), transform).GetComponent<AutoAttack>();
-        autoAttack.Damage = 1;
+        // Position the projectile at the player's position, but outside the player's sprite bounds
+        Vector3 pos = transform.position + mouseDirection * playerBounds.x*2;
+        GameObject go = Instantiate(autoAttackPrefab, pos, Quaternion.Euler(new Vector3(0, 0, 0)));
+        AutoAttack autoAttack = go.GetComponent<AutoAttack>();
+
+        // Set its properties
+        autoAttack.Damage = stats.Damage;
+        autoAttack.Range = stats.Range;
+        autoAttack.Speed = stats.Speed;
         autoAttack.Direction = mouseDirection;
-        autoAttack.Knockback = mouseDirection * attackKnockback;
-        autoAttack.DestroyAfterTime(attackDuration);
+        autoAttack.Knockback = mouseDirection * stats.Knockback;
+
+        // Mark it for destruction after some time
+        autoAttack.DestroyAfterTime(stats.Duration);
     }
 
     public bool CanMove()
     {
-        if(isMovementBlockedByAttack)
-        {
-            return false;
-        }
-        return true;
+        return !isMovementBlockedByAttack;
     }
 
     public bool CanAutoAttack()
     {
-        if(Time.time >= lastAuttoAttackTime + 1f/attackSpeed)
-        {
-            return true;
-        } else
-        {
-            return false;
-        }
+        return Time.time >= lastAuttoAttackTime + 1f/ getAttackStats().Speed;
     }
 
+    // Attack stats for the current weapon level
+    public AttackLevelStats getAttackStats() {
+        return weaponLevelStats[currentWeaponLevel];
+    }
+
+    private AttackLevel getNextWeaponLevel() {
+        switch(currentWeaponLevel) {
+            case AttackLevel.One:
+                return AttackLevel.Two;
+            case AttackLevel.Two:
+                return AttackLevel.Three;
+            case AttackLevel.Three:
+                return AttackLevel.One;
+            default:
+                return AttackLevel.One;
+        }
+    }
 }
