2fd613b3bdea6dabc696cbefe56072d9c2fa23ea
aldegen <colindegen1996@gmail.com>
2022-08-01 21:10:21 +0200

    added tweening to the fireball acceleration

--- Assets/Scripts/AutoAttack.cs
+++ Assets/Scripts/AutoAttack.cs
@@ -7,9 +7,12 @@ public class AutoAttack : MonoBehaviour
     public float Damage { get; set; }
     public Vector3 Knockback { get; set; }
     public Vector3 Direction { get; set; }
+
+    public AnimationCurve acceleration = AnimationCurve.Linear(0, 0, 1, 1);
     public float Range { get; set; }
     public float Speed { get; set; }
     private Vector3 startingPosition;
+    private float passedTimeSinceStart = 0f;
 
     public void Start() {
         // Keep track of starting position
@@ -20,12 +23,14 @@ public class AutoAttack : MonoBehaviour
     }
 
     public void FixedUpdate() {
-        transform.position += Direction * Time.fixedDeltaTime * Speed;
+        transform.position += Direction * Time.fixedDeltaTime * Speed * acceleration.Evaluate(passedTimeSinceStart);
 
         // Destroy if the attack's range is exceeded
         if (Vector3.Distance(startingPosition, transform.position) > Range) {
             Destroy(gameObject);
         }
+
+        passedTimeSinceStart += Time.fixedDeltaTime;
     }
 
 
---
+++ Assets/Scripts/Tweening/Tweening.cs
@@ -0,0 +1,15 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tweening
+{
+    public static IEnumerator Jump(GameObject objectToJump, Vector3 landPosition)
+    {
+        while(true)
+        {
+            
+            yield return null;
+        }
+    }
+}
